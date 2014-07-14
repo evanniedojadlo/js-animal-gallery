b0af1ed80d037acc40c4dc08836168a7368a1e18
evanniedojadlo <evanniedojadlo@gmail.com>
2014-07-14 13:36:43 -0500

    Began adding the required JSON to pull the images into the site.

--- gallery/script.js
+++ gallery/script.js
@@ -1,1 +1,16 @@
 // Add your code here
+
+
+
+
+
+
+
+
+
+
+//Placing at the bottom, adding the rest of the JS at top
+
+$.getJSON( "http://mks-frontend-gallery.herokuapp.com/", function( json ) {
+    console.log(json);
+});
