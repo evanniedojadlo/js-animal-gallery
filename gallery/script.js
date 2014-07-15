7c2b8bdededb4d80d7a8dd135461176733207a67
evanniedojadlo <evanniedojadlo@gmail.com>
2014-07-14 21:08:43 -0500

    Added the example and review from class, need to focus on JSON this week to better understand.

--- gallery/script.js
+++ gallery/script.js
@@ -1,6 +1,39 @@
 // Add your code here
 
-
+$(document).ready(function(){
+	var animals = [];
+ 
+	$.getJSON( "https://mks-frontend-gallery.herokuapp.com/", function( json ) {
+	  for (var i in json) {
+	  	$(".gallery").append("<div class='large-4 small-6 columns " + json[i].animals.join(" ") + "'><img src=" + json[i].url + "></div>");
+	  }
+	  var ids = [];
+	  $.each( json, function ( key, val ) {
+	    
+	    for (var x = 0; x < val.animals.length; x++) {
+	    	ids.push(
+	    		val.animals[x]
+	    	);
+	    }
+ 
+	    $.unique(ids);
+	  });
+	  for (i in ids) {
+	  	$(".filters").append("<div class='filter-button individual button active' id='" + ids[i] + "'>" + ids[i]+ "</div>");
+	  }
+	  console.log(ids);
+	}).done(function(){
+		console.log("done")
+		$('.filter-button').click(function(){
+			$(this).toggleClass("active");
+			$("." + this.id).fadeToggle(1000, "swing", function(){console.log("fade is done")});
+		});
+	}).fail(function(){
+		console.log("fail")
+	});
+ 
+	
+});
 
 
 
@@ -11,6 +44,9 @@
 
 //Placing at the bottom, adding the rest of the JS at top
 
-$.getJSON( "http://mks-frontend-gallery.herokuapp.com/", function( json ) {
-    console.log(json);
-});
+//$.getJSON( "http://mks-frontend-gallery.herokuapp.com/", function( json ) {
+//    console.log(json);
+//});
+
+
+
